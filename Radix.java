45f9e45e98d8c28e5e66fe4d1e16ac10ef301df8
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-04-01 10:58:05 -0400

    biggest helper method created!

--- Radix.java
+++ Radix.java
@@ -1,7 +1,22 @@
 public class Radix{
 
   public static void radixsort(int[] data){
+    MyLinkedList[] bucket = new MyLinkedList[data.length];
+    int count = biggest(data);
+  }
 
+  private static int biggest(int[] data){
+    int max = 0;
+    for(int num: data){
+      int test = num;
+      int count = 0;
+      while(test != 0){
+        count++;
+        test = test / 10;
+      }
+      if(count > max){max = count;}
+    }
+    return max;
   }
 
   public static void main(String args[]){

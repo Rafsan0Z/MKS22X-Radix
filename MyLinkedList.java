de08e4fd2168ec109e9d04cf5bd6dae352dbc3a1
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-04-29 18:16:52 -0400

    node class bought inside!

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -2,7 +2,49 @@
 public class MyLinkedList<E>{
   private int size;
   private Node<E> start,end;
+//Node Class
+  public class Node<E>{
+    private E data;
+    private Node next,prev;
 
+    public Node(Node<E> n, Node<E> p, E d){
+      data = d;
+      next = n;
+      prev = p;
+    }
+
+    public Node<E> next(){ // This get is for the Node
+      return next;
+    }
+
+    public Node<E> prev(){
+      return prev;
+    }
+
+    public E getData(){ // This get is for integer data
+      return data;
+    }
+
+    public void setPrev(Node P){ // Set previous Node
+      prev = P;
+    }
+
+    public void setNext(Node N){ // Set the next Node
+      next = N;
+    }
+
+    public E setData(E i){
+      E result = data;
+      data = i;
+      return result;
+    }
+
+    public String toString(){
+      String result = "" + data;
+      return result;
+    }
+  }
+//End of Node class
   public MyLinkedList(){
     size = 0;
     start = null;
